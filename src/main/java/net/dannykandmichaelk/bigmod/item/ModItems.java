eb2e8728a687a91b1f067721347bf73b669ea8f4
BT_3N3_10 <danielk379@nycstudents.net>
2025-03-05 11:01:03 -0500

    bigcommit

---
+++ src/main/java/net/dannykandmichaelk/bigmod/item/ModItems.java
@@ -0,0 +1,10 @@
+package net.dannykandmichaelk.bigmod.item;
+
+import net.dannykandmichaelk.bigmod.FirstMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, FirstMod.MODID);
+}
