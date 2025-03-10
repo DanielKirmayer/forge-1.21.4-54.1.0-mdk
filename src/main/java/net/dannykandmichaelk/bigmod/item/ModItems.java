c8bf01af4839b9c0d36ea6fffe0e2656dce990e3
BT_2S14_21 <danielk379@nycstudents.net>
2025-03-10 11:03:52 -0400

    Commit 2

--- src/main/java/net/dannykandmichaelk/bigmod/item/ModItems.java
+++ src/main/java/net/dannykandmichaelk/bigmod/item/ModItems.java
@@ -2,9 +2,15 @@ package net.dannykandmichaelk.bigmod.item;
 
 import net.dannykandmichaelk.bigmod.FirstMod;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModItems {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, FirstMod.MODID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, FirstMod.MOD_ID);
+
+
+    public static void register(IEventBus eventbus){
+        ITEMS.register(eventbus);
+    }
 }
